c943d7e0d0fadbf03e5805d6876bbf686d9059b4
yabenatti <min.benatti@gmail.com>
2018-05-11 11:05:39 -0300

    Initial Commit

---
+++ SwiftEntryKitTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftEntryKitTest
+//
+//  Created by Yasmin Benatti on 11/05/18.
+//  Copyright © 2018 Yasmin Benatti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
